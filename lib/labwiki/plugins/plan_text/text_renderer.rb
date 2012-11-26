8c53af42a3ddaea3a6c507eaab4700a50f05f6dc
Max Ott <max.ott@nicta.com.au>
2012-11-26 10:52:59 -0200

    Added a 'url' remapper to the wiki renderer to take care of relative urls in the wiki markup.

--- lib/labwiki/plugins/plan_text/text_renderer.rb
+++ lib/labwiki/plugins/plan_text/text_renderer.rb
@@ -5,11 +5,14 @@ module OMF::Web::Theme
   
   # Override some of the functionality of the text renderer defined in OMF::Web
   class TextRenderer < Erector::Widget
+    include OMF::Common::Loggable
     
     def initialize(text_widget, content, opts)
       super opts
+      @opts = opts
       @widget = text_widget
       @content = content
+      #puts "CONTENT>>>> #{opts.inspect}"
       @content_descriptor = opts[:content].content_descriptor
     end
     
@@ -17,7 +20,7 @@ module OMF::Web::Theme
       link :href => '/plugin/plan_text/css/plan_text.css', :rel => "stylesheet", :type => "text/css"
       wid = "w#{@widget.object_id}"
       div :class => "text plan_text", :id => wid do
-        rawtext @content.to_html
+        rawtext @content.to_html(:img_url_resolver => lambda() {|url| fix_image_url(url)})
       end
       javascript %{
         L.require('#LW.plugin.plan_text.plan_text_monitor', '/plugin/plan_text/js/plan_text_monitor.js', function() {
@@ -25,7 +28,13 @@ module OMF::Web::Theme
         })
       }
     end
-          
+    
+    # Return image url which can be resolved within labwiki
+    def fix_image_url(url)
+      cp = @opts[:content].create_proxy_for_url(url)
+      debug "Resolving #{url} to #{cp.content_url}"
+      cp.content_url
+    end
   end 
 
 end # OMF::Web::Theme
