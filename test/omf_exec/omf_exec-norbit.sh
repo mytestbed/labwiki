ebb04640b1ac73d46a0b3e34adfdb3ed7e2c9ac5
Ubuntu <ubuntu@labwiki.(none)>
2013-07-11 03:29:21 +0000

    Using exec in ec script, stop exp should work now

--- test/omf_exec/omf_exec-norbit.sh
+++ test/omf_exec/omf_exec-norbit.sh
@@ -7,5 +7,5 @@ CWD=$(dirname $0)
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 rvm use ruby-1.8.7
 
-ruby -I $OMF_HOME/omf-expctl/ruby -I $OMF_HOME/omf-common/ruby $OMF_HOME/omf-expctl/ruby/omf-expctl.rb \
+exec ruby -I $OMF_HOME/omf-expctl/ruby -I $OMF_HOME/omf-common/ruby $OMF_HOME/omf-expctl/ruby/omf-expctl.rb \
   -N -C $CWD/etc/omf-expctl.norbit.yaml --log $CWD/etc/omf-expctl_log.xml $*
