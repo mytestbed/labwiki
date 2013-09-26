836acca5c57a194abd6159e70411c94a29882126
Jack C Hong <jhong@nicta.com.au>
2013-09-26 19:00:19 +1000

    Add EC runner for omf v6

---
+++ test/omf_exec/omf_exec_6.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#OMF_HOME=$HOME/omf_6
+OMF_HOME=$HOME/nicta/omf
+CWD=$(dirname $0)
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+rvm use ruby-1.9.3@omf
+
+exec ruby -I $OMF_HOME/omf_common/lib -I $OMF_HOME/omf_ec/lib $OMF_HOME/omf_ec/bin/omf_ec \
+  --uri xmpp://bob --log_config $CWD/etc/ec_6_log.yml exec $*
