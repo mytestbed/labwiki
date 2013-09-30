b587f788c8c4fc7256d80508833ef601930e5ff6
Jack C Hong <jhong@nicta.com.au>
2013-09-30 17:35:29 +1000

    Add oml_uri to ec 6 runner script

--- test/omf_exec/omf_exec_6.sh
+++ test/omf_exec/omf_exec_6.sh
@@ -3,9 +3,10 @@
 #OMF_HOME=$HOME/omf_6
 OMF_HOME=$HOME/nicta/omf
 CWD=$(dirname $0)
+OML_PATH=bob
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 rvm use ruby-1.9.3@omf
 
 exec ruby -I $OMF_HOME/omf_common/lib -I $OMF_HOME/omf_ec/lib $OMF_HOME/omf_ec/bin/omf_ec \
-  --uri xmpp://srv.mytestbed.net --log_config $CWD/etc/ec_6_log.rb exec $*
+  --uri xmpp://srv.mytestbed.net --log_config $CWD/etc/ec_6_log.rb exec --oml_uri=$OML_PATH $*
