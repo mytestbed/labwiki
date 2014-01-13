d075d63af93452d2a3bc66a18f25e1a07895d4b6
Jack C Hong <jhong@nicta.com.au>
2014-01-13 15:47:22 +1100

    Add serialisation to graph desc

--- lib/labwiki/plugins/experiment/graph_description.rb
+++ lib/labwiki/plugins/experiment/graph_description.rb
@@ -2,17 +2,17 @@
 require 'uri'
 
 module LabWiki::Plugin::Experiment
-        
+
   # Hold the description for a graph defined for an experiment
   #
   class GraphDescription < OMF::Base::LObject
-    
+
     attr_reader :name, :type, :mstreams
-    
+
     def parse(type, descr)
       type = type.split(':')
       descr = URI.decode(descr)
-      
+
       debug "parse: #{type}--#{descr}"
       case type[0]
       when 'START'
@@ -35,19 +35,31 @@ module LabWiki::Plugin::Experiment
         warn("Unknown graph description type '#{type.inspect}'")
       end
     end
-    
-    # return a hash describing hte graph suitable for the browser side
+
+    # return a hash describing the graph suitable for the browser side
     # renderer
     #
     def render_option()
       @opts
     end
-        
+
     def initialize
       @mstreams = {}
       @opts = {
         :margin => {:left => 80, :right => 50}
       }
+    end
+
+    def to_json
+      { name: @name, type: @type, opts: @opts, mstreams: @mstreams }.to_json
+    end
+
+    def from_json(json)
+      hash = JSON.parse(json)
+      @name = hash[:name]
+      @type = hash[:type]
+      @opts = hash[:opts]
+      @mstreams = hash[:mstreams]
     end
   end # class
 end # module
