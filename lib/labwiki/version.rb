f3c2edc59cffe4bef50d5bb43fcfd8ceb5a8ff4d
Max Ott <max.ott@nicta.com.au>
2014-04-30 11:52:13 +1000

    We better get the git commit instead of the tag for versioning

--- lib/labwiki/version.rb
+++ lib/labwiki/version.rb
@@ -1,27 +1,29 @@
 
 module LabWiki
   VERSION = [0, 15, 'pre']
+  @@version = nil
 
   def self.version
+    return @@version if @@version
     version = VERSION.join('.')
-    if File.exist? File.absolute_path(File.join(File.dirname(__FILE__), '/../../.git'))
-      git_tag  = `git describe --tags 2> /dev/null`.chomp
-      unless git_tag.empty?
-        version += '-' + git_tag.gsub(/-/, '.').gsub(/^v/, '')
-      end
-    end
-    version
+    @@version = version_append_git_commit(version, File.join(File.dirname(__FILE__), '/../../.git'))
   end
 
   def self.plugin_version(version, init_path = nil)
     version = version.join('.') if version.is_a? Array
     if (init_path)
-      git_dir = File.absolute_path(File.join(File.dirname(init_path), '../../../../.git'))
-      if File.exist? git_dir
-        git_tag  = `git --git-dir #{git_dir} describe --tags 2> /dev/null`.chomp
-        unless git_tag.empty?
-          version += '-' + git_tag.gsub(/-/, '.').gsub(/^v/, '')
-        end
+      git_dir = File.join(File.dirname(init_path), '../../../../.git')
+      version = version_append_git_commit(version, git_dir)
+    end
+    version
+  end
+
+  def self.version_append_git_commit(version, git_dir)
+    git_dir = File.absolute_path(git_dir)
+    if File.exist? git_dir
+      cs = `git --git-dir #{git_dir} log -n 1 2> /dev/null`
+      unless cs.empty?
+        version += '-' + cs[7, 8]
       end
     end
     version
