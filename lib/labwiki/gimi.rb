50a5d22a34b9e972f1a94fcff58e714118f29e9e
Jack C Hong <jhong@nicta.com.au>
2014-06-27 16:53:53 +1000

    Add a new login page for Google OpenId
    It can be enabled via config file option
    
    login:
      page: /resource/login/google_openid.html
      auth_type: OpenID.Google

--- lib/labwiki/gimi.rb
+++ lib/labwiki/gimi.rb
@@ -37,22 +37,28 @@ class LabWiki::Gimi < OMF::Base::LObject
   end
 
   def init_user(user)
-    pretty_name = (user['http://geni.net/user/prettyname'] || ['Unknown']).first
-    OMF::Web::SessionStore[:name, :user] = pretty_name
+    case user["lw:auth_type"]
+    when "OpenID.GENI"
+      pretty_name = user['http://geni.net/user/prettyname'].first
 
-    if urn_a = user['http://geni.net/user/urn']
-      urn = urn_a.first
-      OMF::Web::SessionStore[:urn, :user] = urn.gsub '|', '+'
-      OMF::Web::SessionStore[:id, :user] = urn && urn.split('|').last
+      if (urn = user['http://geni.net/user/urn'].first)
+        OMF::Web::SessionStore[:urn, :user] = urn.gsub '|', '+'
+        OMF::Web::SessionStore[:id, :user] = urn && urn.split('|').last
+      end
+      if (irods_user = user['http://geni.net/irods/username'].first)
+        OMF::Web::SessionStore[:id, :irods_user] = irods_user
+      end
+      if (irods_zone = user['http://geni.net/irods/zone'].first)
+        OMF::Web::SessionStore[:id, :irods_zone] = irods_zone
+      end
+    when "OpenID.Google"
+      last_name = user["http://axschema.org/namePerson/last"].first
+      first_name = user["http://axschema.org/namePerson/first"].first
+      pretty_name = "#{first_name} #{last_name}"
+      OMF::Web::SessionStore[:id, :user] = user["http://axschema.org/contact/email"].first
     end
-    if iu_a = user['http://geni.net/irods/username']
-      irods_user = iu_a.first
-      OMF::Web::SessionStore[:id, :irods_user] = irods_user
-    end
-    if iz_a = user['http://geni.net/irods/zone']
-      irods_zone = iz_a.first
-      OMF::Web::SessionStore[:id, :irods_zone] = irods_zone
-    end
+
+    OMF::Web::SessionStore[:name, :user] = pretty_name || "Unknown"
   end
 
   def init_repo(user_info)
