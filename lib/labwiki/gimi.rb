2dfa7be03413977b33987de113a100148e9ac168
Jack C Hong <jackhongx@gmail.com>
2014-06-30 14:32:49 +1000

    Add try method to all objects
    array.try(:first) avoids typing array && array.first

--- lib/labwiki/gimi.rb
+++ lib/labwiki/gimi.rb
@@ -2,6 +2,7 @@
 
 require 'omf-web/session_store'
 require 'labwiki/plugin_manager'
+require 'labwiki/core_ext/object'
 
 # Class handling all the GIMI specific 'stuff'. Should most likely
 # migrate into a separate plugin
@@ -39,23 +40,23 @@ class LabWiki::Gimi < OMF::Base::LObject
   def init_user(user)
     case user["lw:auth_type"]
     when "OpenID.GENI"
-      pretty_name = user['http://geni.net/user/prettyname'].first
+      pretty_name = user['http://geni.net/user/prettyname'].try(:first)
 
-      if (urn = user['http://geni.net/user/urn'].first)
+      if (urn = user['http://geni.net/user/urn'].try(:first))
         OMF::Web::SessionStore[:urn, :user] = urn.gsub '|', '+'
         OMF::Web::SessionStore[:id, :user] = urn && urn.split('|').last
       end
-      if (irods_user = user['http://geni.net/irods/username'].first)
+      if (irods_user = user['http://geni.net/irods/username'].try(:first))
         OMF::Web::SessionStore[:id, :irods_user] = irods_user
       end
-      if (irods_zone = user['http://geni.net/irods/zone'].first)
+      if (irods_zone = user['http://geni.net/irods/zone'].try(:first))
         OMF::Web::SessionStore[:id, :irods_zone] = irods_zone
       end
     when "OpenID.Google"
-      last_name = user["http://axschema.org/namePerson/last"].first
-      first_name = user["http://axschema.org/namePerson/first"].first
+      last_name = user["http://axschema.org/namePerson/last"].try(:first)
+      first_name = user["http://axschema.org/namePerson/first"].try(:first)
       pretty_name = "#{first_name} #{last_name}"
-      OMF::Web::SessionStore[:id, :user] = user["http://axschema.org/contact/email"].first
+      OMF::Web::SessionStore[:id, :user] = user["http://axschema.org/contact/email"].try(:first)
     end
 
     OMF::Web::SessionStore[:name, :user] = pretty_name || "Unknown"
