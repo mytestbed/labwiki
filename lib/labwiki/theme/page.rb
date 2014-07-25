d708dae9bba9b879a781db09bc7a0cf3912ef8db
Jack C Hong <jack.hong@nicta.com.au>
2014-07-25 16:53:15 +1000

    Delete gimi.rb and code related to Exp Context

--- lib/labwiki/theme/page.rb
+++ lib/labwiki/theme/page.rb
@@ -15,22 +15,7 @@ module OMF::Web::Theme
     depends_on :css, "/resource/theme/labwiki/css/kaiten.css"
     depends_on :css, "/resource/theme/labwiki/css/labwiki.css"
 
-    # depends_on :js, '/resource/vendor/jquery/jquery.periodicalupdater.js'
-    # depends_on :js, "/resource/vendor/jquery-ui/js/jquery-ui.min.js"
-    #depends_on :js, "/resource/vendor/jquery-ui/js/jquery.ui.autocomplete.js"
-
-    #depends_on :js, "/resource/theme/labwiki/js/column_controller.js"
-    #depends_on :js, "/resource/theme/labwiki/js/content_selector_widget.js"
-    #depends_on :js, "/resource/theme/labwiki/js/execute_col_controller.js"
-    #depends_on :js, "/resource/theme/labwiki/js/labwiki.js"
-
-    # Pretend experiment context doesn't exist for a moment
-    #depends_on :js, "/resource/theme/labwiki/js/exp_context.js"
-
     depends_on :js, "/resource/vendor/bootstrap/js/bootstrap.js"
-    #depends_on :js, '/resource/vendor/jquery/jquery.js'
-    #depends_on :js, '/resource/vendor/jquery/jquery.periodicalupdater.js'
-    #depends_on :js, '/resource/vendor/jquery-ui/js/jquery-ui.min.js'
     depends_on :js, '/resource/vendor/jquery-ui/js/jquery-ui.js'
     depends_on :js, '/resource/vendor/jquery.ui.touch-punch.min.js'
     depends_on :js, '/resource/vendor/smartmenus/jquery.smartmenus.js'
@@ -84,17 +69,6 @@ module OMF::Web::Theme
               end
             end
 
-            unless LabWiki::Configurator[:stable]
-              if LabWiki::Configurator[:stable_instance_location]
-                span :class => 'label label-warning', :style => "font-size: 110%; line-height: 29px; margin-left: 10px;" do
-                  text "This is NOT the stable instance. You could visit "
-                  a :href => LabWiki::Configurator[:stable_instance_location] do
-                    text "the stable instance here."
-                  end
-                end
-              end
-            end
-
             ul class: 'nav navbar-nav navbar-right' do
               li do
                 a id: 'tools-menu-a', class: 'nav-menu', href: "#" do
@@ -104,7 +78,6 @@ module OMF::Web::Theme
                 ul id: 'tools-menu-ul', class: "dropdown-menu" do
                   li 'GIMI', class: "dropdown-header"
                   if (authorisation_info = LabWiki::Configurator[:session][:authorisation])
-                    #authorisation_info[:certificate] = LabWiki::Configurator.read_file(authorisation_info[:cert_file])
                     authorisation_info[:certificate] = LabWiki::Configurator.read_file('session/authorisation/cert_file')
                     li do
                       form method: "post", action: authorisation_info[:url], id: "authorise" do
@@ -138,52 +111,6 @@ module OMF::Web::Theme
           div :id => "k-slider" do
             @col_renderers.each do |renderer|
               rawtext renderer.to_html
-            end
-          end
-        end
-      end
-
-      div id: "new-exp-modal", class: "modal fade" do
-        div class: "modal-dialog" do
-          div class: "modal-content" do
-
-            div class: "modal-header" do
-              button :type => "button", :class => "close", :"data-dismiss" => "modal", :"aria-hidden" => "true" do
-                rawtext '&times;'
-              end
-              h3 "New experiment context", style: "font-size: 20px;"
-            end
-
-            div class: "modal-body" do
-              form class: "form-horizontal", role: "form" do
-                div class: "form-group" do
-                  label "Project", class: "col-sm-3 control-label"
-                  div class: "col-sm-9" do
-                    select id: "project", class: "form-control" do
-                      if OMF::Web::SessionStore[:projects, :geni_portal]
-                        OMF::Web::SessionStore[:projects, :geni_portal].each do |p|
-                          option p[:name], value: p[:name]
-                        end
-                      end
-                    end
-                  end
-                end
-
-                div class: "form-group" do
-                  label class: "col-sm-3 control-label" do
-                    text "Name"
-                  end
-                  div class: "col-sm-9" do
-                    input id: "exp-name", type: "text", class: "form-control"
-                    input id: "irods-user-name", type: "hidden", value: OMF::Web::SessionStore[:id, :user]
-                  end
-                end
-              end
-            end
-
-            div class: "modal-footer" do
-              a "Close", :href => "#", :class => "btn btn-default", :"data-dismiss" => "modal"
-              a "Save", :href => "#", :id => "save-exp", :class => "btn btn-primary", :"data-dismiss" => "modal"
             end
           end
         end
